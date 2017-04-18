685b0ee06d003e27d6c03616cdbda595bd60469f
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-04-18 09:31:45 +0200

    adding more information to the output of nick's house

--- 02_VariablesAssignmentsFunctions/code/nick_area.py
+++ 02_VariablesAssignmentsFunctions/code/nick_area.py
@@ -6,14 +6,14 @@ def area(base, side, height):
 
 
 area1 = area(5, 3, 2)  # should be approx. 20
-print(area1)
+print('Area 1:', area1)
 area2 = area(2, 5, 1)  # should be approx. 11
-print(area2)
+print('Area 2:', area2)
 area3 = area(3, 3, 3)  # should be approx. 13.5
-print(area3)
+print('Area 3:', area3)
 
 random_side = random.randint(2, 5)
 random_base = random.randint(random_side, 5)  # Constraint
 random_height = random.randint(1, 3)  # Small roof
 
-print(area(random_base, random_side, random_height))
+print('Random area:', area(random_base, random_side, random_height))
