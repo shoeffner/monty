4fd1fdab51bf5a4fa0f6f6e0b13bae37674926c5
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-05-13 18:47:16 +0200

    errata in __init__.py

--- 06_Packages/code/solution/mazesolver/__init__.py
+++ 06_Packages/code/solution/mazesolver/__init__.py
@@ -1,2 +1,31 @@
-import mazesolver.io
-import mazesolver.solver
+# Errata:
+# Instead of using these two statements:
+#
+#     import mazesolver.io
+#     import mazesolver.solver
+#
+# It is besser to use the directory relative statements:
+
+from . import io
+from . import solver
+
+# This basically allows to import mazesolver in the following directory
+# structure:
+#
+#     working_directory
+#     - solution
+#         - mazesolver
+#             - __init__.py
+#             - io.py
+#             - solver.py
+#
+# With the above mentioned imports, it's impossible to do:
+#
+#    import solution.mazesolver
+#
+# The corrected version with the dot notation (from . import...) allows this.
+#
+# For more information, please refer to PEP 328:
+#
+#     https://www.python.org/dev/peps/pep-0328/
+#
