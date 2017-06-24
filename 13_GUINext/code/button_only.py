ea41d9ec93339209123f6b89b0854c8fa1b13e56
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-06-24 18:25:35 +0200

    renaming master to root

--- 13_GUINext/code/button_only.py
+++ 13_GUINext/code/button_only.py
@@ -1,13 +1,13 @@
 from tkinter import Tk, Button
 
-
 class SimpleWindow:
 
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
+    def __init__(self, root):
+        self.root = root
+        root.title("A simple GUI")
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(root, text="Close",
+                                   command=root.quit)
         self.close_button.pack()
 
 if __name__ == '__main__':
